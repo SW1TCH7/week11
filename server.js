58095b92573a73ca12b242c10277d40edb545be7
Daniel Ojala <ojaladanik@gmail.com>
2026-03-10 20:36:16 +0200

    week 12 ingr add and delete

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const recipesRouter = require('./routes/recipes.routes');
 const ingredientsRouter = require('./routes/ingredients.routes');
 const fullRecipesRouter = require('./routes/fullrecipes.router');
 const randomRouter = require('./routes/randomRecipe.routes');
+app.use(express.json());
 
 app.use('/recipes', recipesRouter);
 app.use('/ingredients', ingredientsRouter);
 app.use('/fullrecipes', fullRecipesRouter);
 app.use('/random', randomRouter);
+//app.use();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
